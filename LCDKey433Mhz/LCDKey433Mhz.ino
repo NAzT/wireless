9137a2ec25838474f561da77720df4bbfae38b47
Nat <nat.wrw@gmail.com>
2013-12-13 16:12:46 +0700

    Refactor

--- LCDKey433Mhz/LCDKey433Mhz.ino
+++ LCDKey433Mhz/LCDKey433Mhz.ino
@@ -64,20 +64,12 @@ void charDisplay() {
     sensor_t *sensor_ptr; 
     // display characters mode
     lcd.setCursor(0, 0);
-    lcd.print("Type: ");
-    lcd.print(sensor_page%NUM_SENSOR);
-    lcd.print(" ");
-    lcd.print("Node: ");
-    lcd.print(sensor_nid%MAX_NODE);
-    // lcd.setCursor(14,0);
-    // lcd.write(sensor_page);
+    lcd.print("Type: "); lcd.print(sensor_page%NUM_SENSOR); lcd.print(" "); lcd.print("Node: "); lcd.print(sensor_nid%MAX_NODE);
       
     lcd.setCursor(0,1);
-    // lcd.print("Vert: ");
-    // lcd.print(sensor_nid);
-    // lcd.print(printStr);
     sensor_ptr = sensor_data[sensor_page%NUM_SENSOR][sensor_nid%MAX_NODE];
-    if (sensor_ptr == 0) {
+    
+    if (sensor_ptr == NULL) {
       lcd.print("NULL            ");
     }
     else {
@@ -88,27 +80,18 @@ void charDisplay() {
  }
 
 void buttonUp(eDFRKey key) {
-  //Serial.print("KEY: ");
-  //Serial.print(key);
-  // Serial.println(brightness);
-
   switch(key) {
     case eUp:
       if (DEBUG) {
         Serial.println("eUp");
       }
       sensor_nid = abs(--sensor_nid);
-      // if(brightness < 255) 
-      //   keypad.setBrightness(++brightness);
       break;
     case eDown:
       ++sensor_nid;
       if (DEBUG) {
         Serial.println("eDown");
       }      
-      //Serial.println("eDown");    
-      // if(brightness > 0) 
-      //   keypad.setBrightness(--brightness);
       break;
       
     case eLeft:
@@ -116,7 +99,11 @@ void buttonUp(eDFRKey key) {
       if (DEBUG) {
         Serial.println("eLeft");
       }      
+
+      // the previous page 
       --sensor_page;
+
+      // reset sensor nid to zero
       sensor_nid = 0;
       break;
       
@@ -124,25 +111,23 @@ void buttonUp(eDFRKey key) {
       if (DEBUG) {
         Serial.println("eRight");
       }    
-      //Serial.println("eRight");        
-      sensor_nid = 0;
+      // the next page
       ++sensor_page;
+      // reset sensor id to zero
+      sensor_nid = 0;
       break;
       
     case eSelect:
       if (DEBUG) {
         Serial.println("eSelect");
       }    
-      //Serial.println("eSelect: ");        
+
+      // toggle active sensors
       ++select_pressed;
-      //Serial.print(select_pressed);
-      //Serial.print(":");
-      //Serial.print(sensor_link_num);
-      //Serial.print("=");
-      //Serial.println(select_pressed%sensor_link_num);
       sensor_t* tmp = sensor_link[select_pressed%sensor_link_num];
       sensor_page = tmp->type;
       sensor_nid = tmp->node;
+
       charDisplay();  
       // switch to mouse mode
       // keypad.setButtonHeldHandler(0);
@@ -157,7 +142,7 @@ void setup() {
   Serial.begin(9600);
   Serial.println("SETUP");
   // Initialise the IO and ISR
-  vw_set_rx_pin(13);    
+  vw_set_rx_pin(2);    
   vw_set_ptt_inverted(true); // Required for DR3100
   vw_setup(2000);	 // Bits per sec
   vw_rx_start();       // Start the receiver PLL running  
@@ -186,14 +171,14 @@ void loop() {
 
   if (vw_get_message(buf, &buflen)) // Non-blocking    
   {
-//      digitalWrite(13, true); // Flash a light to show received good message
+      digitalWrite(13, true); // Flash a light to show received good message
+
       int l_type;
       int l_node;
       char* l_value;
-      sensor_t *tmpSensor;
-      sensor_t *sensor_temp;
 
-      //Serial.println((char*) buf);
+      sensor_t *temp_sensor;
+      sensor_t *sensor_temp;
 
       //SPLIT DATA WITH ,
       char *a = (char*) buf;
@@ -201,13 +186,17 @@ void loop() {
           words[count++] = aPtr;
       }
 
+      // grab a node & type of sensors
       l_type = atoi(words[1]);
       l_node = fromBinary(words[0]);
 
       sensor_temp = sensor_data[l_type][l_node];
-      if (sensor_temp != 0) {
+
+      // free memory
+      if (sensor_temp != NULL) {
         free(sensor_data[l_type][l_node]->str);
       }
+
       free(sensor_data[l_type][l_node]);
 
       if (DEBUG) {
@@ -219,11 +208,11 @@ void loop() {
         Serial.println(words[2]);
       }
 
-      tmpSensor = (sensor_t*)malloc(sizeof(sensor_t));
-      tmpSensor->node = l_node;
-      tmpSensor->type = l_type;
-      tmpSensor->str = strdup(words[2]);
-      sensor_data[l_type][l_node] = tmpSensor;
+      temp_sensor = (sensor_t*)malloc(sizeof(sensor_t));
+      temp_sensor->node = l_node;
+      temp_sensor->type = l_type;
+      temp_sensor->str = strdup(words[2]);
+      sensor_data[l_type][l_node] = temp_sensor;
        
       if (DEBUG) {
         Serial.println("==============================");
@@ -280,7 +269,7 @@ void loop() {
       if (DEBUG) {
         Serial.println("===========================");
       }
-//      digitalWrite(13, false); // Flash a light to show received good message
+      digitalWrite(13, false); // Flash a light to show received good message
   }
 
   charDisplay();  
